a19dd1fa0f909c0359dbd34c87d6bb5e4adcffce
stellawroblewski <43860837+stellawroblewski@users.noreply.github.com>
2018-10-07 16:39:46 -0700

    Update q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -1,9 +1,10 @@
+//this function takes two inputs, a base and exponent, and computers the power function result recursively
 #include <iostream>
 using namespace std;
 
-int pow(int a, int b)
+int pow(int a, int b)	//takes two inputs, a base and exponent, and computers the power function result recursively
 {
-	if (b != 0) {
+	if (b != 0) {	//while b > 0
 		return (a*pow(a, b-1));
 	} else {
 		return 1;
@@ -11,15 +12,15 @@ int pow(int a, int b)
 }
 
 int main() {
-	int base;
-	cin >> base;
+	int base;	
+	cin >> base;	//read in base value
   int exponent;
-	cin >> exponent;
-	if ((base >= 1) & (exponent >= 1)) {
+	cin >> exponent;	//read in exponent value
+	if ((base >= 1) & (exponent >= 1)) {	//if both > 0, call pow and save result in result
 	  int result = pow(base, exponent);
-    cout << result << endl;
+    cout << result << endl;	//return result
   } else {
-    cout << "Both inputs must be positive." << endl;
+    cout << "Both inputs must be positive." << endl; //return string if either input is negative
     return 0;
   }
 return 0; 
