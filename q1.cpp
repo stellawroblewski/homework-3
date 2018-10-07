c7a05e14deec8ca14ebd313d0c7292f61e7b8d2d
stellawroblewski <43860837+stellawroblewski@users.noreply.github.com>
2018-10-07 16:31:19 -0700

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,26 +1,27 @@
+//this function reads 3 integer values and returns the greatest value
 #include <iostream>
 using namespace std;
 
-int main()
+int main()	//this function reads 3 integer values and returns the greatest value
 {
-	int firstInput;
-	cin >> firstInput;
+	int firstInput;		
+	cin >> firstInput;	//reads in first int
 	int secondInput;
-	cin >> secondInput;
+	cin >> secondInput;	//reads in second int
 	int thirdInput;
-	cin >> thirdInput;
+	cin >> thirdInput;	//reads in third int
 	int greatest = 0;
-	if (firstInput >= secondInput) {
-		greatest = firstInput;
+	if (firstInput >= secondInput) { //compares first and second int
+		greatest = firstInput;	// if first int larger, changes greatest to first int value
 	} else {
-		greatest = secondInput;
+		greatest = secondInput; // if second int larger, changest greatest to second int value
 	}
-	if (thirdInput >= greatest) {
-		greatest = thirdInput;
+	if (thirdInput >= greatest) { 
+		greatest = thirdInput;	// if third int larger than current value, changes greatest to third int value
 	} else {
 		; 
 	}
-	cout << "Greatest value is: " << greatest << endl;
+	cout << "Greatest value is: " << greatest << endl;	//returns greatest value
 	
 	return 0;
 }
